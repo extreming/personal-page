09ae149cc58e3aab186f2831529d7a468db5fb15
wangtl <tianlun_wang@outlook.com>
2024-07-01 22:14:08 +0800

    添加技能logo，开发技能详情

---
+++ src/components/common/TechDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col gap-4 items-center" @click="openUrl(url)">
+    <img :src="logo" alt="">
+    <span>{{label}}</span>
+  </div>
+</template>
+
+<script setup>
+defineProps(['logo', 'label', 'url']);
+
+const openUrl = function(url) {
+  window.open(url, '_blank')
+}
+</script>
--- src/components/sections/Skills.vue
+++ src/components/sections/Skills.vue
@@ -11,6 +11,10 @@ import Typography from '../common/Typography.vue';
       <Typography variant="subtitle" class="text-center">
         The skills, tools and technologies I am really good at:
       </Typography>
+      <!-- 技能图标链接 -->
+      <div class="grid grid-cols-3 gap-y-4 md:grid-cols-6 lg:grid-cols-8 lg:gap-y-12">
+        <div class="w-[150px] h-[100px] border" v-for="item in 16"></div>
+      </div>
     </div>
   </Container>
 </template>
